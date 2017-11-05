6c077b89d219d381420e9bf86d6a01736f1e7ec4
Jose Angel <jagracia19@gmail.com>
2017-11-05 17:29:41 +0100

    Starfield

---
+++ Starfield/uFormStars.pas
@@ -0,0 +1,82 @@
+unit uFormStars;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  System.Math.Vectors, FMX.Types3D, FMX.Controls3D, FMX.MaterialSources,
+  FMX.Objects3D, FMX.Viewport3D;
+
+type
+  TFormStars = class(TForm)
+    Viewport3D1: TViewport3D;
+    Camera1: TCamera;
+    Light1: TLight;
+    DummyScene: TDummy;
+    SphereStar: TSphere;
+    LightMaterialSource1: TLightMaterialSource;
+    ProxyObject1: TProxyObject;
+    Timer1: TTimer;
+    procedure FormCreate(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+  private
+    function RandomLocation: TPoint3D;
+  public
+  end;
+
+var
+  FormStars: TFormStars;
+
+implementation
+
+{$R *.fmx}
+
+{ TFormStars }
+
+procedure TFormStars.FormCreate(Sender: TObject);
+const STARS_COUNT = 100;
+var I: Integer;
+    star: TProxyObject;
+begin
+  Randomize;
+  for I := 0 to STARS_COUNT-1 do
+  begin
+    star := TProxyObject.Create(DummyScene);
+    star.SourceObject := SphereStar;
+    star.Parent := DummyScene;
+    star.Position.Point := RandomLocation;
+  end;
+end;
+
+function TFormStars.RandomLocation: TPoint3D;
+const
+  MAX_X = 50;
+  MAX_Y = 50;
+  MAX_Z = 200;
+begin
+  Result.X := -MAX_X + Random * 2 * MAX_X;
+  Result.Y := -MAX_Y + Random * 2 * MAX_Y;
+  Result.Z := Random * MAX_Z;
+end;
+
+procedure TFormStars.Timer1Timer(Sender: TObject);
+const DELTA_Z = 2;
+var I   : Integer;
+    ctrl: TControl3D;
+    obj : TFmxObject;
+begin
+  for I := 0 to DummyScene.ChildrenCount-1 do
+  begin
+    obj := DummyScene.Children[I];
+    if obj is TControl3D then
+    begin
+      ctrl := TControl3D(obj);
+      ctrl.Position.Z := ctrl.Position.Z - DELTA_Z;
+      if ctrl.Position.Z < 0 then
+        ctrl.Position.Point := RandomLocation;
+    end;
+  end;
+end;
+
+end.
